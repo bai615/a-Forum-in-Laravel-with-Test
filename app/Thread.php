9cc345e9091fd8719fc7a65b90625b1d756fafb7
baihua@sndskj.com <baihua2018>
2018-07-24 15:01:36 +0800

    2.测试话题

--- app/Thread.php
+++ app/Thread.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Thread extends Model
 {
     protected $guarded = []; // 意味所有属性均可更新，后期会修复此安全隐患
+
+    public function path()
+    {
+        return '/threads/'.$this->id;
+    }
 }
---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_browse_threads()
+    {
+        /*$response = $this->get('/threads');
+        $response->assertStatus(200);*/
+
+        /*$thread = factory('App\Thread')->create();
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);*/
+
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
+}
